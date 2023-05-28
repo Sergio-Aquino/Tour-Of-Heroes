614d641486e5919cd1453b3eef6c36b2c1684547
Sergio <sergioadm120@gmail.com>
2023-05-28 18:20:30 -0300

    feature: Get data from a server

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Hero} from "./hero";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+      { id: 12, name: 'Black Widow' },
+      { id: 13, name: 'Iron Man' },
+      { id: 14, name: 'Hulk' },
+      { id: 15, name: 'Thor' },
+      { id: 16, name: 'Doctor Strange' },
+      { id: 17, name: 'Loki' },
+      { id: 18, name: 'Captain America' },
+      { id: 19, name: 'SpiderMan' },
+      { id: 20, name: 'Black Panther' }
+    ];
+    return {heroes};
+  }
+
+  genId(heroes: Hero[]): number {
+    return heroes.length > 0 ? Math.max(...heroes.map(hero => hero.id)) + 1 : 11;
+  }
+}
+
